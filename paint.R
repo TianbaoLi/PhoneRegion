6a3b9d4007950a67f74b52ac360a6ed8bbf79f2e
TuringMacLee <turingmac@hotmail.com>
2015-07-23 11:09:30 +0800

    Heatmap with pinyin

--- paint.R
+++ paint.R
@@ -17,4 +17,8 @@ midpos <- function(x) mean(range(x,na.rm=TRUE))
 centres <- ddply(china_data,.(NAME),colwise(midpos,.(long,lat)))
 province_city <- read.csv("/home/turingmac/R-3.2.1/chinamap/chinaprovincecity.csv")	#读取省会城市坐标
 capital<- transform(province_city, province = iconv(province, from = 'UTF-8'))
-ggplot(china_data,aes(long,lat))+geom_polygon(aes(group=group,fill=USER),colour="grey60")+scale_fill_gradient(low="white",high="steelblue")+coord_map("polyconic")+geom_text(aes(x = jd,y = wd,label = province), data =capital)+theme(panel.grid = element_blank(),panel.background = element_blank(),axis.text = element_blank(),axis.ticks = element_blank(),axis.title = element_blank())
+pinyin<-read.csv("/home/turingmac/PhoneRegion/pinyin.csv")	#读取指标数据，csv格式
+capital<-join(capital, pinyin, type="full")	#合并两个数据框
+G1=ggplot(china_data,aes(long,lat))+geom_polygon(aes(group=group,fill=USER),colour="grey60")+scale_fill_gradient(low="white",high="steelblue")+coord_map("polyconic")+geom_text(aes(x = jd,y = wd,label = PINYIN), data =capital)+theme(panel.grid = element_blank(),panel.background = element_blank(),axis.text = element_blank(),axis.ticks = element_blank(),axis.title = element_blank())
+G1
+ggsave(G1, file="/home/turingmac/PhoneRegion/heatmap.pdf",scale=4)
